21d91778786620da168bfdeea848ae32a8b2dff2
nicotsessilede <nicotsessilede12@gmail.com>
2022-05-16 13:19:13 +0100

    sas

--- dev-env/provider.tf
+++ dev-env/provider.tf
@@ -8,7 +8,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "cloud-shell-storage-westeurope"
     storage_account_name = "csb10032001f9f70371"
-    container_name       = "backend"
+    #container_name       = "backend"
     key                  = "dev.terraform.tfstate"
   }
 }
